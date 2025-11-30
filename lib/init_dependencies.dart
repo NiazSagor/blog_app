04f35d586b3f4565de89467c6a371747b4245ac1
NiazSagor <niazsagor@gmail.com>
2025-11-30 13:34:52 +0600

    refactor: Implement dependency injection with get_it
    This commit refactors the application to use the `get_it` package for dependency injection, centralizing service registration and improving modularity.
    
    - Adds the `get_it` dependency to `pubspec.yaml`.
    - Creates a new `init_dependencies.dart` file to handle the setup and registration of all dependencies.
    - Registers `AuthRemoteDataSource`, `AuthRepository`, `UserSignUpUseCase`, and `AuthBloc` as factory or lazy singleton services.
    - Initializes Supabase within the dependency setup and registers the client as a lazy singleton.
    - Simplifies `main.dart` by removing manual dependency instantiation and instead using the service locator to provide the `AuthBloc` to the widget tree.
    
    Took 31 minutes

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,36 @@
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/secrets/app_secrets.dart';
+import 'features/auth/data/repository/auth_repository_impl.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(supabaseClient: serviceLocator()),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory(() => UserSignUpUseCase(serviceLocator()));
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(signUpUseCase: serviceLocator()),
+  );
+}
